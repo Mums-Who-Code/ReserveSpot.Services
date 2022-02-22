b8c0114863aad30e504e9f88683b61e159e5d395
Mahalakshmi Murari <murarimaha@outlook.com>
2022-02-22 13:55:23 -0800

    CONTROLLER: Added Home Controller

---
+++ ReserveSpot.Services.Api/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace ReserveSpot.Services.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> get() =>
+            Ok("Hurry Up! Reserve Your Spot. We are happy to see you playing sports.");
+    }
+}
