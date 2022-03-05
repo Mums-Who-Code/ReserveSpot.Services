0ebbb9bff5eda52e26198098432f4243a7dbfb48
Mahalakshmi Murari <murarimaha@outlook.com>
2022-03-04 21:16:48 -0800

    INFRA: Initialize Acceptane Tests

---
+++ ReserveSpot.Services.Tests.Acceptance/Apis/Home/HomeApiTests.Logic.cs
@@ -0,0 +1,28 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace ReserveSpot.Services.Tests.Acceptance.Apis.Home
+{
+    public partial class HomeApiTests
+    {
+        [Fact]
+        public async Task ShouldReturnHomeMessageAsync()
+        {
+            //given
+            string expectedHomeMessage =
+                 "Hurry Up! Reserve Your Spot. We are happy to see you playing sports.";
+
+            //when
+            string actualHomeMessage =
+                 await this.reserveSpotApiBroker.GetHomeMessageAsync();
+
+            //then
+            actualHomeMessage.Should().BeEquivalentTo(expectedHomeMessage);
+        }
+    }
+}
---
+++ ReserveSpot.Services.Tests.Acceptance/Apis/Home/HomeApiTests.cs
@@ -0,0 +1,18 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using ReserveSpot.Services.Tests.Acceptance.Brokers;
+using Xunit;
+
+namespace ReserveSpot.Services.Tests.Acceptance.Apis.Home
+{
+    [Collection(nameof(ApiTestCollection))]
+    public partial class HomeApiTests
+    {
+        private readonly ReserveSpotApiBroker reserveSpotApiBroker;
+
+        public HomeApiTests(ReserveSpotApiBroker reserveSpotApiBroker) =>
+            this.reserveSpotApiBroker = reserveSpotApiBroker;
+    }
+}
