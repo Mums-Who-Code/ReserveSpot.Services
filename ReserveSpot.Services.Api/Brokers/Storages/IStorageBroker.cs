b4b6a73dcfc22bdb0c7205ae3ad50defa80a79b7
Mahalakshmi Murari <murarimaha@outlook.com>
2022-03-14 13:11:13 -0700

    BROKER: Initialize Storage Broker

---
+++ ReserveSpot.Services.Api/Brokers/Storages/IStorageBroker.cs
@@ -0,0 +1,10 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+namespace ReserveSpot.Services.Api.Brokers.Storages
+{
+    public interface IStorageBroker
+    {
+    }
+}
---
+++ ReserveSpot.Services.Api/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,27 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ReserveSpot.Services.Api.Brokers.Storages
+{
+    public class StorageBroker : EFxceptionsContext, IStorageBroker
+    {
+        private IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = this.configuration.GetConnectionString(name: "DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
