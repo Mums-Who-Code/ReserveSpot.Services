5ddb24b4dcc722819038b521c99f31203c9bd937
Mahalakshmi Murari <murarimaha@outlook.com>
2022-03-25 21:12:02 -0700

    DATA: User Model & Seeding

---
+++ ReserveSpot.Services.Api/Migrations/20220322014922_SeedUsers.cs
@@ -0,0 +1,40 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReserveSpot.Services.Api.Migrations
+{
+    public partial class SeedUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { new Guid("4c54b038-c653-4904-a33f-54e60ca4d531"), "Admin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
