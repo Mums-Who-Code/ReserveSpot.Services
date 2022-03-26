cfa2412fa817c413afa6ba89b436e225e6baa204
Mahalakshmi Murari <murarimaha@outlook.com>
2022-03-26 16:57:00 -0700

    CODERUB: DATA: User Model and Migration

--- ReserveSpot.Services.Api/Brokers/Storages/StorageBroker.User.Seeds.cs
+++ ReserveSpot.Services.Api/Brokers/Storages/StorageBroker.User.Seeds.cs
@@ -14,7 +14,6 @@ namespace ReserveSpot.Services.Api.Brokers.Storages
         {
             var serviceAdminUser = new User
             {
-                //Id = Guid.Parse("80aea9ae-967a-4718-b04e-97d2614b862b")
                 Id = Guid.Parse("4c54b038-c653-4904-a33f-54e60ca4d531"),
                 Name = "Admin"
             };
