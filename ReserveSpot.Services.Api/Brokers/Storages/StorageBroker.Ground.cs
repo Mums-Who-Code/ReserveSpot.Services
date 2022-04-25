b0677c766bca0d4d864069499769c88431438bb9
Mahalakshmi Murari <murarimaha@outlook.com>
2022-04-25 11:45:17 -0700

    BROKER: Insert Category

--- ReserveSpot.Services.Api/Brokers/Storages/StorageBroker.Ground.cs
+++ ReserveSpot.Services.Api/Brokers/Storages/StorageBroker.Ground.cs
@@ -2,7 +2,9 @@
 // Copyright (c) MumsWhoCode. All rights reserved.
 // ------------------------------------------------
 
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using ReserveSpot.Services.Api.Models.Grounds;
 
 namespace ReserveSpot.Services.Api.Brokers.Storages
@@ -10,5 +12,16 @@ namespace ReserveSpot.Services.Api.Brokers.Storages
     public partial class StorageBroker
     {
         DbSet<Ground> Grounds { get; set; }
+
+        public async ValueTask<Ground> InsertGroundAsync(Ground ground)
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            EntityEntry<Ground> entityEntry = await broker.Grounds.AddAsync(ground);
+
+            await broker.SaveChangesAsync();
+
+            return entityEntry.Entity;
+        }
     }
 }
