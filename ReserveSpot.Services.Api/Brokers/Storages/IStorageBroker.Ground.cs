6057f0dc0e2cd9ef9956946ded3d9fb1e2fa9329
Mahalakshmi Murari <murarimaha@outlook.com>
2022-04-25 11:30:23 -0700

    BROKER: Insert Ground

--- ReserveSpot.Services.Api/Brokers/Storages/IStorageBroker.Ground.cs
+++ ReserveSpot.Services.Api/Brokers/Storages/IStorageBroker.Ground.cs
@@ -2,9 +2,13 @@
 // Copyright (c) MumsWhoCode. All rights reserved.
 // ------------------------------------------------
 
+using System.Threading.Tasks;
+using ReserveSpot.Services.Api.Models.Grounds;
+
 namespace ReserveSpot.Services.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<Ground> InsertGroundAsync(Ground ground);
     }
 }
